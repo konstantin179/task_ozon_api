3734f4c5c7c82b7b922ea01640ca2888a3f48e67
Konstantin Lobov <kostialobov@gmail.com>
2022-10-26 14:02:27 +0300

    changed reports.py

--- reports.py
+++ reports.py
@@ -2,7 +2,18 @@ import os
 import requests
 import time
 import urllib.request
+from pathlib import Path
+import psycopg2
+import zipfile
 import csv
+
+
+connection = psycopg2.connect(dbname="postgres_db", user="postgres")
+cursor = connection.cursor()
+cursor.execute("SELECT client_id, client_secret FROM client_info;")
+CLIENT_ID, CLIENT_SECRET = cursor.fetchone()
+cursor.close()
+connection.close()
 
 
 class Report:
@@ -14,7 +25,6 @@ class Report:
                            "grant_type": "client_credentials"}
         self.token_time = -1800
         self.token = None
-        self.headers = {"Authorization": self.token, }
 
     def get_report(self, date_from, date_to, report_type):
         uuid = self._request_report(date_from, date_to, report_type)
@@ -24,14 +34,52 @@ class Report:
                 break
             time.sleep(2)
         link = self._get_link(uuid)
-        try:
-            response = requests.get(link)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            raise Exception("Error: " + str(e))
+        file_extension = link[-4:]
+        filename = uuid + file_extension
+        tmp_path = Path("tmp")
+        tmp_path.mkdir(exist_ok=True)
+        file_path = tmp_path / filename
         some_url = ''
-        with urllib.request.urlopen(some_url + link) as f:
-            report = csv.reader(f.read().decode('utf-8'))
+        urllib.request.urlretrieve(some_url + link, file_path)
+        if file_extension == '.zip':
+            zipdir_path = file_path.with_suffix('')
+            zipdir_path.mkdir()
+            with zipfile.ZipFile(file_path, mode="r") as archive:
+                archive.extractall(zipdir_path)
+            csv_files = zipdir_path.glob('*')
+            for file in csv_files:
+                self.add_csvfile_to_db(file)
+                os.remove(file)
+            os.rmdir(zipdir_path)
+            os.remove(file_path)
+        else:
+            self.add_csvfile_to_db(file_path)
+            os.remove(file_path)
+
+    @staticmethod
+    def add_csvfile_to_db(filepath):
+        conn = psycopg2.connect(dbname="postgres_db", user="postgres")
+        cur = conn.cursor()
+        table_name = filepath.stem
+        with open(filepath, 'r', encoding="utf-8") as f:
+            reader = csv.reader(f)
+            # Headers without id
+            headers = next(reader)[1:]
+        query_info = [table_name].extend(headers)
+        # Change the query to match the actual file!
+        cur.execute("""CREATE TABLE %s (
+            id integer PRIMARY KEY,
+            %s text,
+            %s text,
+            %s text
+        );
+        """, query_info)
+        with open(filepath, 'r', encoding="utf-8") as f:
+            next(f)  # Skip the header row.
+            cur.copy_from(f, table_name, sep=',')
+        conn.commit()
+        cur.close()
+        conn.close()
 
     def _request_report(self, date_from, date_to, report_type):
         self._update_token()
